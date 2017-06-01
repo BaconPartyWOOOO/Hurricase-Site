88183995c11e117a8469f7a1757259dfe3773a87
Steven Johnson <sjsushiman@gmail.com>
2017-06-01 19:01:03 -0400

    Add initial spring project

---
+++ src/main/java/hurricase/hurricasesite/HurricaseSiteApplication.java
@@ -0,0 +1,12 @@
+package hurricase.hurricasesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurricaseSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurricaseSiteApplication.class, args);
+	}
+}
