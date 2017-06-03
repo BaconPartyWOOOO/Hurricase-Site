dff807bc6bc83fdb9bf9117973ef51382540a9ec
Steven Johnson <sjsushiman@gmail.com>
2017-06-03 14:10:20 +0900

    Add dialogs

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -8,3 +8,32 @@ app.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('grey');
 });
+
+app.controller('AppCtrl', function($scope, $mdDialog) {
+    $scope.showAlert = function(ev) {
+        $mdDialog.show(
+          $mdDialog.alert()
+            .clickOutsideToClose(true)
+            .textContent('Your cart is Empty')
+            .ariaLabel('Empty Cart')
+            .ok('Okay')
+            .targetEvent(ev)
+        );
+    };
+
+    $scope.showConfirm = function(ev) {
+        var confirm = $mdDialog.confirm()
+              .clickOutsideToClose(true)
+              .title('Unable to order product')
+              .textContent('Due to immense popularity, the Hurricase is currently unavailable. Please check back when we get more in stock!')
+              .ariaLabel('Sold out')
+              .targetEvent(ev)
+              .ok('Will do!')
+              .cancel('Not Interested');
+
+        $mdDialog.show(confirm).then(function() {
+        }, function() {
+        });
+      };
+
+});
